b43dc9f223ceb1909bccbaff27b066aa54eaca22
Vikram Chatterjee <strycora@gmail.com>
2021-04-02 16:17:10 -0600

    added gems rack-cors and pry, updated puma for localhost:3001, un-nested workouts from sessions, and added seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :inspirations
   resources :bests
-  
-  resources :sessions do 
-    resources :workouts
-  end
+  resources :workouts
+  resources :sessions 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+s = Session.create(name: 'Chest Day 1', date:Date.today)
+
+workout = Workout.create(name: 'Bench Press', sets: 3, reps: 10, weight: 225, session_id: 1)
+workout_two= Workout.create(name: 'Pec Fly', sets: 5, reps: 8, weight: 80, session_id: 1)
